bddeeccb61e2e51590c820df7791396d91d32b7e
Stephen Huenneke <stephen.huenneke@gmail.com>
2013-05-16 17:10:50 -0700

    Initial version, no tests yet though.

---
+++ memcache_pool.py
@@ -0,0 +1,114 @@
+import logging
+import pickle
+from pymemcache.client import Client
+from gevent.queue import Queue
+
+
+def pickle_serializer(key, value):
+    if type(value) == str:
+        return value, 1
+    return pickle.dumps(value), 2
+
+
+def pickle_deserializer(key, value, flags):
+    if flags == 1:
+        return value
+    if flags == 2:
+        return pickle.loads(value)
+    raise Exception("Unknown serialization format")
+
+
+class PyMemcachePool(object):
+    """
+    Synchronized pool for memcached clients
+    """
+    def __init__(self, host=None, port=None, serializer=pickle_serializer, deserializer=pickle_deserializer,
+                 maxsize=100, timeout=15):
+        """
+        Setup a new empty pool
+        """
+        if not isinstance(maxsize, (int, long)):
+            raise Exception('Maxsize must be an integer or long, got: %s' % (maxsize, ))
+        if not isinstance(timeout, (int, long)):
+            raise Exception('Timeout must be an integer or long, got: %s' % (timeout, ))
+        self._host = host
+        self._port = port
+        self._maxsize = maxsize
+        self._timeout = timeout
+        self._serializer = serializer
+        self._deserializer = deserializer
+        self._pool = Queue()
+        self._size = 0
+
+    def set_host_and_port(self, host, port):
+        self._host = host
+        self._port = port
+
+    def get_client(self):
+        """
+        Get an existing client, or if none are available create a new
+        one. If the pool is at maxsize, return nothing.
+        """
+        pool = self._pool
+        logging.debug("Attempting to get client from pool size=%s queue_size=%s" % (self._size, pool.qsize()))
+        if self._size >= self._maxsize or pool.qsize():
+            client = pool.get(timeout=2)
+            try:
+                client._connect()
+            except:
+                logging.error("Re-connect to memcached failed!")
+                if client is not None:
+                    client.close()
+                self._size -= 1
+                return self.get_client()
+            else:
+                return client
+        else:
+            logging.debug("Createing new client")
+            self._size += 1
+            try:
+                new_item = self._create_client()
+            except:
+                logging.error("Unable to connect to memcached!")
+                self._size -= 1
+                raise
+            return new_item
+
+    def return_client(self, client):
+        """
+        Return a client to the pool
+        """
+        if client is not None:
+            try:
+                client._connect()
+            except:
+                logging.debug("Returned bad connection to pool")
+                self._size -= 1
+            else:
+                logging.debug("Returned OK client")
+                self._pool.put(client)
+
+    def shutdown(self):
+        """
+        Shutdown the clients in the pool.
+        """
+        while not self._pool.empty():
+            client = self._pool.get_nowait()
+            try:
+                client.close()
+            except Exception:
+                logging.error("Unable to close client")
+                pass
+
+    def _create_client(self):
+        """
+        Create a new client using smart_client to get connection details
+        """
+        logging.debug("Creating new memcached client")
+        try:
+            client = Client((self._host, self._port), serializer=self._serializer, deserializer=self._deserializer)
+            client._connect()
+        except:
+            logging.error("Unable to connect to memcached!")
+            client = None
+        return client
